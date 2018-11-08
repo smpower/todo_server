8f283553ed4ab7ddc5b647846bd0a18d04c1df40
smpower <rf.wangchn@gmail.com>
2018-11-08 17:57:34 +0800

    链接数据库并测试删除和查询

--- app.js
+++ app.js
@@ -5,8 +5,15 @@
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var app = express();
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'ruofei',
+  password: 'rf.wangchn',
+  database: 'todo'
+});
 
 // all environments
 app.set('port', 8080);
@@ -53,6 +60,38 @@ app.post('/todo/test/post', function(req, res, next) {
       message: 'Hello world, again!'
     }
   });
+});
+
+app.post('/todo/register', function(req, res, next) {
+  const addSql = 'INSERT INTO user(username, email, password) VALUES(?, ?, ?)';
+  const addSqlParams = [req.body.username, req.body.email, req.body.password];
+  console.log(req.body);
+
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'ruofei',
+    password : 'rf.wangchn',
+    database : 'todo'
+  });
+
+  connection.connect();
+
+  // connection.query('INSERT INTO user(username, email, password) VALUES(?, ? ,?)', [req.body.username, req.body.email, req.body.password],
+  connection.query(addSql, addSqlParams,
+    function(error, results, fields) {
+      if (error) throw error;
+      console.log(results);
+      console.log(fields);
+      res.json(fields);
+    });
+
+  // connection.query('SELECT * FROM user', function (error, results, fields) {
+  //   if (error) throw error;
+  //   console.log(results);
+  //   res.json(results);
+  // });
+
+  connection.end();
 });
 
 http.createServer(app).listen(app.get('port'), function() {
