193238d287fce4e2c1191ca99d9ba42018566821
smpower <rf.wangchn@gmail.com>
2018-11-12 15:34:09 +0800

    测试跨域

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // 允许 Express 跨域
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Method', 'PUT,POST,GET,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Content-Type', 'application/json;charset=UTF-8');
